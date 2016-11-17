ad1b35e3031c90ec167431d799bcefcd9efa12e6
Marco Mangan <marco.mangan@pucrs.br>
2016-11-17 20:06:55 -0200

    Clean up

--- monopoly/src/test/java/br/pucrs/fds/monopoly/AppTest.java
+++ monopoly/src/test/java/br/pucrs/fds/monopoly/AppTest.java
@@ -1,18 +1,11 @@
 package br.pucrs.fds.monopoly;
 
 import static org.junit.Assert.assertTrue;
-
 import org.junit.*;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest 
 {
 
-    /**
-     * Rigourous Test :-)
-     */
 	@Test	
     public void testApp()
     {
